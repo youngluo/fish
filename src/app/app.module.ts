103a5a0411349d91dca074084dbaaa24de005e01
young <ly532265997@gmail.com>
2017-12-18 18:18:56 +0800

    add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { OperationPanelComponent } from './components/operation-panel/operation-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WorkspaceComponent,
+    OperationPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/components/operation-panel/operation-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operation-panel',
+  templateUrl: './operation-panel.component.html',
+  styleUrls: ['./operation-panel.component.scss']
+})
+export class OperationPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
