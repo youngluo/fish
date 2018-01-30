8da75d4c0216a5934943ec546af7f6a7814b441d
young <ly532265997@gmail.com>
2018-01-30 18:53:18 +0800

    添加入口页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import routes from './config/routes.js';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { OperationPanelComponent } from './components/operation-panel/operation-panel.component';
+import { IndexComponent } from './pages/index/index.component';
+import { TemplateComponent } from './pages/template/template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WorkspaceComponent,
-    OperationPanelComponent
+    OperationPanelComponent,
+    IndexComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NgZorroAntdModule.forRoot()
+    NgZorroAntdModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config/routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { IndexComponent } from '../pages/index/index.component';
+import { TemplateComponent } from '../pages/template/template.component';
+
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'template',
+    component: TemplateComponent
+  },
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+];
+
+export default routes;
